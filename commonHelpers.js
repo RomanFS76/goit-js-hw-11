283ec21f1e117b1262a816eac60d06141bcb88e9
RomanFS76 <36919661+RomanFS76@users.noreply.github.com>
2024-04-12 19:55:33 +0000

    Deploying to gh-pages from @ RomanFS76/goit-js-hw-11@fb9cc6f16ef836e58eb832c2f7944ec2d2ac234c 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,12 +1,34 @@
-import{S as l,i as c}from"./assets/vendor-8c59ed88.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))s(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const n of t.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&s(n)}).observe(document,{childList:!0,subtree:!0});function o(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function s(e){if(e.ep)return;e.ep=!0;const t=o(e);fetch(e.href,t)}})();function u(i){const r="43330031-9673f4a92262d12e3841226eb",o=new URLSearchParams({key:r,q:i,image_type:"photo",orientation:"horizontal",safesearch:!0,per_page:15});return fetch(`https://pixabay.com/api/?${o}`).then(s=>{if(!s.ok)throw new Error(s.status);return s.json()})}const f=new l(".gallery a",{});f.refresh();function m(i){return i.map(({webformatURL:r,largeImageURL:o,tags:s,likes:e,views:t,comments:n,downloads:p})=>`
+import{S as c,i as u}from"./assets/vendor-8c59ed88.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const o of t.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&i(o)}).observe(document,{childList:!0,subtree:!0});function s(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function i(e){if(e.ep)return;e.ep=!0;const t=s(e);fetch(e.href,t)}})();function p(l){const r="43330031-9673f4a92262d12e3841226eb",s=new URLSearchParams({key:r,q:l,image_type:"photo",orientation:"horizontal",safesearch:!0,per_page:15});return fetch(`https://pixabay.com/api/?${s}`).then(i=>{if(!i.ok)throw new Error(i.status);return i.json()})}const d=new c(".gallery a");function f(l){return l.map(({webformatURL:r,largeImageURL:s,tags:i,likes:e,views:t,comments:o,downloads:n})=>`
         <li class="gallery-item">
-          <a class="gallery-link" href="${o}">
+          <a class="gallery-link" href="${s}">
               <img 
                 class="gallery-image" 
                 src="${r}"
-                alt="${s}" 
+                alt="${i}"
+                width = "1000"
+                height = "800"
               />
           </a>
+          <div class="gallery-desc">
+            <ul class="gallery-desc-list">
+              <li class="gallery-desc-item">
+                <p>Likes</p>
+                <p>${e}</p>
+              </li>
+              <li class="gallery-desc-item">
+                <p>Views</p>
+                <p>${t}</p>
+              </li>
+              <li class="gallery-desc-item">
+                <p>Comments</p>
+                <p>${o}</p>
+              </li>
+              <li class="gallery-desc-item">
+                <p>Downloads</p>
+                <p>${n}</p>
+              </li>
+            </ul>
+          </div>
         </li>
-      `).join("")}const a=document.querySelector(".form-search"),d=document.querySelector(".gallary");a.addEventListener("submit",i=>{i.preventDefault();const r=i.target.elements.input.value;u(r).then(o=>{if(r===""){c.show({message:"Field must be filled!",color:"green",position:"center",timeout:2e3});return}d.insertAdjacentHTML("beforeend",m(o.hits))}).catch(o=>console.log(o)),a.reset()});
+      `).join("")}d.refresh();const a=document.querySelector(".form-search"),m=document.querySelector(".gallary");a.addEventListener("submit",l=>{l.preventDefault();const r=l.target.elements.input.value;p(r).then(s=>{if(r===""){u.show({message:"Field must be filled!",color:"green",position:"center",timeout:2e3});return}s.hits,m.insertAdjacentHTML("beforeend",f(s.hits))}).catch(s=>console.log(s)),a.reset()});
 //# sourceMappingURL=commonHelpers.js.map
