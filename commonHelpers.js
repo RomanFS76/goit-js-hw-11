f8cc93d732de4c4e4f4ca4cbcd7b7372200432ab
RomanFS76 <36919661+RomanFS76@users.noreply.github.com>
2024-04-12 20:19:38 +0000

    Deploying to gh-pages from @ RomanFS76/goit-js-hw-11@e4d641a5dceba657718afdf15eeb752b9729121a 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,10 +1,10 @@
-import{S as c,i as u}from"./assets/vendor-8c59ed88.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const o of t.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&i(o)}).observe(document,{childList:!0,subtree:!0});function s(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function i(e){if(e.ep)return;e.ep=!0;const t=s(e);fetch(e.href,t)}})();function p(l){const r="43330031-9673f4a92262d12e3841226eb",s=new URLSearchParams({key:r,q:l,image_type:"photo",orientation:"horizontal",safesearch:!0,per_page:15});return fetch(`https://pixabay.com/api/?${s}`).then(i=>{if(!i.ok)throw new Error(i.status);return i.json()})}const d=new c(".gallery a");function f(l){return l.map(({webformatURL:r,largeImageURL:s,tags:i,likes:e,views:t,comments:o,downloads:n})=>`
+import{S as u,i as n}from"./assets/vendor-8c59ed88.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))o(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const l of t.addedNodes)l.tagName==="LINK"&&l.rel==="modulepreload"&&o(l)}).observe(document,{childList:!0,subtree:!0});function r(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function o(e){if(e.ep)return;e.ep=!0;const t=r(e);fetch(e.href,t)}})();function p(i){const s="43330031-9673f4a92262d12e3841226eb",r=new URLSearchParams({key:s,q:i,image_type:"photo",orientation:"horizontal",safesearch:!0,per_page:15});return fetch(`https://pixabay.com/api/?${r}`).then(o=>{if(!o.ok)throw new Error(o.status);return o.json()})}const m=new u(".gallery a");function f(i){return i.map(({webformatURL:s,largeImageURL:r,tags:o,likes:e,views:t,comments:l,downloads:a})=>`
         <li class="gallery-item">
-          <a class="gallery-link" href="${s}">
+          <a class="gallery-link" href="${r}">
               <img 
                 class="gallery-image" 
-                src="${r}"
-                alt="${i}"
+                src="${s}"
+                alt="${o}"
                 width = "1000"
                 height = "800"
               />
@@ -21,14 +21,14 @@ import{S as c,i as u}from"./assets/vendor-8c59ed88.js";(function(){const r=docum
               </li>
               <li class="gallery-desc-item">
                 <p>Comments</p>
-                <p>${o}</p>
+                <p>${l}</p>
               </li>
               <li class="gallery-desc-item">
                 <p>Downloads</p>
-                <p>${n}</p>
+                <p>${a}</p>
               </li>
             </ul>
           </div>
         </li>
-      `).join("")}d.refresh();const a=document.querySelector(".form-search"),m=document.querySelector(".gallary");a.addEventListener("submit",l=>{l.preventDefault();const r=l.target.elements.input.value;p(r).then(s=>{if(r===""){u.show({message:"Field must be filled!",color:"green",position:"center",timeout:2e3});return}s.hits,m.insertAdjacentHTML("beforeend",f(s.hits))}).catch(s=>console.log(s)),a.reset()});
+      `).join("")}m.refresh();const c=document.querySelector(".form-search"),g=document.querySelector(".gallary");c.addEventListener("submit",i=>{i.preventDefault();const s=i.target.elements.input.value;p(s).then(r=>{if(s===""){n.show({message:"Field must be filled!",color:"green",position:"center",timeout:2e3});return}console.log(Object.keys(r.hits).length),console.log(r.hits),Object.keys(r.hits).length===0&&n.show({message:"Sorry, there are no images matching your search query. Please try again!",color:"blue",position:"center",timeout:2e3}),g.insertAdjacentHTML("beforeend",f(r.hits))}).catch(r=>console.log(r)),c.reset()});
 //# sourceMappingURL=commonHelpers.js.map
